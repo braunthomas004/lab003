c0d5a67be481f6db78916c4b1f07ff6343d84f29
Thomas Braun <braunthomas004@gmail.com>
2018-10-03 21:27:41 -0400

    First attempt.

---
+++ WebApplication1/wwwroot/function.js
@@ -0,0 +1,5 @@
+function showReceipt() {
+	document.getElementById("inputName").innerHTML = document.getElementById("custName").value;
+	document.getElementById("inputBev").innerHTML = document.getElementById("bevChoice").value;
+	document.getElementById("bevAmount").innerHTML = document.getElementById("beveQuantity").value;
+}
